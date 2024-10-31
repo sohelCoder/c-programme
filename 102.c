f08ba47c3cf9ce9e334e2b95076fbdddbbc84983
sohelCoder <sohelmujwar2005@gmail.com>
2024-10-31 15:54:19 +0530

    even odd cheking progrmme

---
+++ 102.c
@@ -0,0 +1,12 @@
+#include<stdio.h> 
+int main () {
+int number;
+printf(" enter number : ");
+scanf("%d",&number);
+if ( number %2 == 0 ) {
+printf("the number  %d is even :\n  ", number) ;
+ }
+else {
+printf("the number %d is odd : \n ",number);	
+}
+}
