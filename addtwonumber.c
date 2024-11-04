5fbfc8a5c6990bc25c3071cdee510a6ccaa942b5
sohelCoder <sohelmujwar2005@gmail.com>
2024-11-04 17:33:40 +0530

    add,sub,mul,divi

---
+++ addtwonumber.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main () {
+int a=20,b=80,c,d,e,f;
+c=a+b;
+printf(" sum %d\n : ",c);
+d=a-b;
+printf("subtraction %d\n : ",d);
+e=a*b;
+printf(" multiply %d\n : ",e);
+f=a/b;
+printf(" division %d\n : ",f);
+}
