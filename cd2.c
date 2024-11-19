15cd45330f38ebe8f39770ab23bd85a4a548e368
sohelCoder <sohelmujwar2005@gmail.com>
2024-11-19 21:31:50 +0530

    hollow star

---
+++ cd2.c
@@ -0,0 +1,34 @@
+
+#include <stdio.h>
+
+int main()
+{
+	int i, space, n = 5, j = 0;
+
+
+
+
+	for (i = 0; i < n - 1; i++) {
+
+		
+		for (space = 1; space < n - i; space++) {
+			printf(" ");
+		}
+		
+		
+				
+		for (j = 0; j <= 2 * i; j++) {
+			if (j == 0 || j == 2 * i)
+				printf("*");
+			else
+				printf(" ");
+		}
+	
+		printf("\n");
+	}
+	
+	for (i = 0; i < 2 * n - 1; i++) {
+		printf("*");
+	}
+	return 0;
+}
