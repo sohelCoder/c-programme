0b56bc52b676377e60dfc31643dd640c7906ed0c
sohelCoder <sohelmujwar2005@gmail.com>
2025-01-04 23:31:47 +0530

    sum...................

---
+++ sohel99.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main () {
+int a,b,c,d,x,y; 
+printf(" enter the first  number : x");
+scanf("%d",&x);
+printf(" enter the second  number : y");
+scanf("%d",&y);
+a=x+y;
+printf("sum is : %d\n",a);
+b=x-y;
+printf("subtraction is : %d\n",b);
+c=x*y;
+printf(" multiply : %d\n ",c);
+d=x/y;
+printf("division: %d\n ",d);
+}
