c601b1c54544bb5ef9f81360389d3285587adedf
sohelCoder <sohelmujwar2005@gmail.com>
2025-01-14 17:36:46 +0530

    sum,divi,mul,sub

---
+++ sum100.c
@@ -0,0 +1,16 @@
+#include<stdio.h> 
+int main () {
+int a,b,c,d,x,y;
+printf("enter the frist number ",x); 
+scanf("%d",&x);
+printf("enter the second number ",y); 
+scanf("%d",&y);
+a=x+y;
+printf("sum : %d\n",a);
+b=x-y;
+printf("subtraction : %d\n",b); 
+c=x/y;
+printf("division : %d\n",c);
+d=x*y;
+printf("multiplication : %d\n",d);
+}
